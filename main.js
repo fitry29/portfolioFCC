4cccd06ebb48e7babc83bf0ce17ed289ed77315e
Fitry <mohdfitri2324@gmail.com>
2023-05-23 00:19:06 +0800

    create website with css js

---
+++ main.js
@@ -0,0 +1,12 @@
+function scrollNavbar(){
+    document.addEventListener('scroll', () => {
+        const navbar = document.getElementById('navbar');
+
+        if(window.scrollY > 0){
+            navbar.classList.add('scrolled');
+        }
+        else{
+            navbar.classList.remove('scrolled');
+        }
+    });
+}
